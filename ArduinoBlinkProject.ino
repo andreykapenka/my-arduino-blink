300aa1e93e4d4e2da47eb051d156459e9c4a5fd3
andreykapenka <alinahismatullina102007@gmail.com>
2026-02-16 14:59:39 +0300

    feat: add SOS signal function

--- ArduinoBlinkProject.ino
+++ ArduinoBlinkProject.ino
@@ -1,9 +1,35 @@
+void blinkSOS() {
+  for (int i = 0; i < 3; i++) {  
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+    delay(200);
+  }
+  delay(400);
+  
+  for (int i = 0; i < 3; i++) {   
+    digitalWrite(13, HIGH);
+    delay(600);
+    digitalWrite(13, LOW);
+    delay(600);
+  }
+  delay(400);
+  
+  for (int i = 0; i < 3; i++) {   
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+    delay(200);
+  }
+  delay(2000);  
+}
 void setup() {
   pinMode(13, OUTPUT);
   pinMode(12, OUTPUT);
 }
 
 void loop() {
+  blinkSOS();
   digitalWrite(13, HIGH);
   delay(500);
   digitalWrite(13, LOW);
